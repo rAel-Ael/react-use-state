a85a4a6c4d4876dca5e229008e6f9b81997c9be5
rAel-Ael <alediru9267@gmail.com>
2026-04-30 16:31:58 +0200

    work in progress: aggiunte card e bottoni

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,13 @@
+function Buttons() {
+    return <> <div>
+            <button type="button" className="btn btn-primary">HTML</button>
+            <button type="button" className="btn btn-primary">CSS</button>
+            <button type="button" className="btn btn-primary">Javascript</button>
+            <button type="button" className="btn btn-primary">Node.js</button>
+            <button type="button" className="btn btn-primary">Express</button>
+            <button type="button" className="btn btn-primary">React</button>
+        </div>
+    
+    </>
+}
+export default Buttons;
